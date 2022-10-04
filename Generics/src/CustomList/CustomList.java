d826e4c118ef4f4f71fdb09f29e4e1cc5106fbfe
mnidchenko <maria.nidchenko@dxc.com>
2022-10-04 17:44:15 +0300

    Complete 7

---
+++ Generics/src/CustomList/CustomList.java
@@ -0,0 +1,56 @@
+package CustomList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomList<T extends Comparable>{
+    private List<T> values;
+
+    public CustomList() {
+        this.values = new ArrayList<>();
+    }
+
+    public void add(T element) {
+        this.values.add(element);
+    }
+
+    public void remove(int index) {
+        this.values.remove(index);
+    }
+
+    public boolean contains(T element) {
+        return this.values.contains(element);
+    }
+
+    public void swap(int firstIndex, int secondIndex) {
+        T firstItem = this.values.get(firstIndex);
+        T secondItem = this.values.get(secondIndex);
+
+        this.values.set(secondIndex, firstItem);
+        this.values.set(firstIndex, secondItem);
+    }
+
+    public int countGreaterThan(T element) {
+        return (int) this.values.stream().filter(e -> e.compareTo(element) > 0).count();
+    }
+
+    public T max() {
+        return this.values.stream().max(T::compareTo).get();
+    }
+
+    public T min() {
+        return this.values.stream().min(T::compareTo).get();
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+
+        for (int i = 0; i < this.values.size(); i++) {
+            output.append(this.values.get(i)).append("\n");
+        }
+        output.deleteCharAt(output.length() - 1);
+        return output.toString();
+    }
+}
---
+++ Generics/src/CustomList/Main.java
@@ -0,0 +1,45 @@
+package CustomList;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        CustomList<String> customList = new CustomList<String>();
+
+        String command = scanner.nextLine();
+        while (!command.equals("END")) {
+            String[] tokens = command.split(" ");
+            switch (tokens[0]) {
+                case "Add":
+                    customList.add(tokens[1]);
+                    break;
+                case "Remove":
+                    customList.remove(Integer.parseInt(tokens[1]));
+                    break;
+                case "Max":
+                    System.out.println(customList.max());
+                    break;
+                case "Min":
+                    System.out.println(customList.min());
+                    break;
+                case "Greater":
+                    System.out.println(customList.countGreaterThan(tokens[1]));
+                    break;
+                case "Swap":
+                    customList.swap(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
+                    break;
+                case "Contains":
+                    System.out.println(customList.contains(tokens[1]));
+                    break;
+                case "Print":
+                    System.out.println(customList);
+                    break;
+                default:
+                    break;
+            }
+            command = scanner.nextLine();
+        }
+    }
+}
