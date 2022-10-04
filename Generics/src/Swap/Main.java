330a786711cf65b01d3a8cd1d6b7530243f8485a
mnidchenko <maria.nidchenko@dxc.com>
2022-10-04 16:24:54 +0300

    Edited to complete 4

--- Generics/src/Swap/Main.java
+++ Generics/src/Swap/Main.java
@@ -7,10 +7,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
-        Box<String> box = new Box<>();
+        Box<Integer> box = new Box<>();
 
         for (int i = 0; i < n; i++) {
-            String line = scanner.nextLine();
+            int line = Integer.parseInt(scanner.nextLine());
             box.add(line);
         }
         int[] nums = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
